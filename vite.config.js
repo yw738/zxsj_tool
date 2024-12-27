a92884c91bfd2a074a2e5d37cf647806993ce818
https://github.com/yw738 <https://github.com/yw738>
2024-12-27 11:43:13 +0800

    配置修复

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ import { VantResolver } from "@vant/auto-import-resolver";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/zxsj_tool/",
+  build: {
+    outDir: "docs"
+  },
   plugins: [
     plugin(),
     Components({
